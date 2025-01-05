7006765acd514db52d616e8fad79382441ee894a
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-05 15:56:53 +0800

    add .gitignore and remove generated target files

--- sandbox-stuff/target/debug/deps/progamming_concepts-2ed478c75ba1ad82.d
+++
@@ -1,9 +0,0 @@
-/home/itscrownie/code/rusty/sandbox-stuff/target/debug/deps/libprogamming_concepts-2ed478c75ba1ad82.rmeta: src/main.rs src/shapes/mod.rs src/shapes/area.rs src/shapes/circle.rs src/shapes/rect.rs
-
-/home/itscrownie/code/rusty/sandbox-stuff/target/debug/deps/progamming_concepts-2ed478c75ba1ad82.d: src/main.rs src/shapes/mod.rs src/shapes/area.rs src/shapes/circle.rs src/shapes/rect.rs
-
-src/main.rs:
-src/shapes/mod.rs:
-src/shapes/area.rs:
-src/shapes/circle.rs:
-src/shapes/rect.rs:
